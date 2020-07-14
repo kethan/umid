2ee134e387a2e03945873c83ab331d2ca23f5e2e
Kethan Surana <kchandrakanth@thapovan-inc.com>
2020-07-14 11:35:22 +0530

    Readme changes

--- examples/simple.js
+++ examples/simple.js
@@ -3,24 +3,31 @@ const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 let m = new Middleware();
 
+// Plain middleware
 m.use((context, next) => {
 	console.log("M1 context", context);
 	context.exec = "M1 executed";
 	next();
-}).use(async (context, next) => {
+})
+
+// Async await middleware
+.use(async (context, next) => {
 	await sleep(2000);
 	console.log("M2 context", context);
 	context.exec = "M2 executed in 2 seconds";
 	next();
 });
 
+// Async await middleware
 let m3 = async (context, next) => {
-	await sleep(1000);
+	await sleep(500);
+	await sleep(500);
 	console.log("M3 context", context);
 	context.exec = "M3 executed in 1 second";
 	next();
 };
 
+// settimeout middleware
 let m4 = (context, next) => {
 	setTimeout(() => {
 		console.log("M4 context", context);
@@ -29,6 +36,7 @@ let m4 = (context, next) => {
 	}, 2000);
 };
 
+// Promise middleware
 let m5 = async (context, next) => {
 	Promise.resolve().then(() => {
 		console.log("M5 context", context);
@@ -37,6 +45,7 @@ let m5 = async (context, next) => {
 	});
 };
 
+// Error middleware
 let errorMiddleware = async (context, next) => {
 	throw "got error!!";
 	next();
